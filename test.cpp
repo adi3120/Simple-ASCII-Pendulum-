84282c5387c47b34da5f721f12b09ca7959a1642
Aditya yadav <83342917+adi3120@users.noreply.github.com>
2021-06-26 20:07:53 +0530

    Fixed many things pendulum works now

--- test.cpp
+++ test.cpp
@@ -3,32 +3,33 @@
 #include <Windows.h>
 #include <vector>
 using namespace std;
-#define pi 3.1415
+
 // These two variables control the size of the window.
-int xSize = 100;
-int ySize = 100;
+const int xSize = 100;
+const int ySize = 100;
 
 // These two variables control the position of the pendulum within the window.
-int offsetx = 25;
-int offsety = -32;
+const int offsetx = 25;
+const int offsety = -32;
 
-// This is the gravity constant.
-float g = 1;
-
+// This is the gravity and PI constant.
+const float g = 0.01;
+const double pi = 3.14;
 // These two variables control the time-step and dampening, respectively.
-float dt = 0.3;
-float DAMPENING = 0.997;
+const float dt = 0.3;
+const float DAMPENING = 0.997;
 
-// These variables handle writing characters directly to the console.
+//These variables handle writing characters directly to the console.
 HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 DWORD dwWritten;
 HWND myconsole = GetConsoleWindow();
 HDC dc = GetDC(myconsole);
 
+
 struct Pendulum
 {
-    float length = 20;
-    float angle = pi / 2;
+    float length = 25;
+    float angle = pi / 3;
     float x = length * sin(angle);
     float y = length * cos(angle);
     float v = 0;
@@ -36,7 +37,7 @@ struct Pendulum
     float m = 2;
 };
 
-void plotLineLow(vector<pair<int,int>> &v ,int x0, int y0, int x1, int y1)
+void plotLineLow(vector<pair<int, int>> &v, int x0, int y0, int x1, int y1)
 {
     int dx = x1 - x0, dy = y1 - y0, yi = 1;
     if (dy < 0)
@@ -53,13 +54,14 @@ void plotLineLow(vector<pair<int,int>> &v ,int x0, int y0, int x1, int y1)
         if (D > 0)
         {
             y += yi;
-            D -= 2 * dx;
+            D = D + (2 * (dy - dx));
         }
-        D += 2 * dy;
+        else
+            D += 2 * dy;
     }
 }
 
-void plotLineHigh(vector<pair<int,int>> &v ,int x0, int y0, int x1, int y1)
+void plotLineHigh(vector<pair<int, int>> &v, int x0, int y0, int x1, int y1)
 {
     int dx = x1 - x0, dy = y1 - y0, xi = 1;
     if (dx < 0)
@@ -76,16 +78,15 @@ void plotLineHigh(vector<pair<int,int>> &v ,int x0, int y0, int x1, int y1)
         if (D > 0)
         {
             x += xi;
-            D -= 2 * dy;
+            D += 2 * (dx - dy);
         }
         D += 2 * dx;
     }
 }
-
-vector<pair<int,int>> drawLine(int x0, int y0, int x1, int y1)
+//Bresenham's algorithm for drawing line
+vector<pair<int, int>> giveCoords(int x0, int y0, int x1, int y1)
 {
     vector<pair<int, int>> v;
-    //sorting
     if (x0 > x1)
     {
         int t;
@@ -96,35 +97,33 @@ vector<pair<int,int>> drawLine(int x0, int y0, int x1, int y1)
         y0 = y1;
         y1 = t;
     }
-    // if(x0==x1){
-    //     int min=y0,max=y1;
-    //     if(y1<y0)
-	// 	{
-	// 		min=y1;
-	// 		max=y0;
-	// 	}
-	// 	for(int i=min;i<=max;i++)
-	// 		v.push_back(make_pair(x0,i));//platno[i][A]=c;
-    // }
-    // if(y0==y1)
-	// {
-	// 	for(int i=x0;i<=x1;i++)
-	// 		v.push_back(make_pair(i,y0));//platno[B][i]=c;
-	// }
-
     if (abs(y1 - y0) < abs(x1 - x0))
-        plotLineLow(v,x0, x1, y0, y1);
+    {
+        if (x0 > x1)
+        {
+            plotLineLow(v, x1, y1, x0, y0);
+        }
+        else
+        {
+            plotLineLow(v, x0, y0, x1, y1);
+        }
+    }
     else
     {
         if (y0 > y1)
-            plotLineHigh(v,x1, y1, x0, y0);
+        {
+            plotLineHigh(v, x1, y1, x0, y0);
+        }
         else
-            plotLineHigh(v,x0, y0, x1, y1);
+        {
+            plotLineHigh(v, x0, y0, x1, y1);
+        }
     }
     return v;
 }
 void clrscr()
 {
+    Sleep(20);
     COORD topLeft = {0, 0};
     DWORD cCharsWritten, dwConSize;
     CONSOLE_SCREEN_BUFFER_INFO cInfo;
@@ -135,7 +134,7 @@ void clrscr()
     dwConSize = cInfo.dwSize.X * cInfo.dwSize.Y;
 
     // Fill the whole screen with blank chars
-    // FillConsoleOutputCharacter(hConsoleOutput, (TCHAR)' ', dwConSize, topLeft, &cCharsWritten);
+    FillConsoleOutputCharacter(hConsoleOutput, (TCHAR)' ', dwConSize, topLeft, &cCharsWritten);
 
     // Get the current text attribute
     GetConsoleScreenBufferInfo(hConsoleOutput, &cInfo);
@@ -146,27 +145,55 @@ void clrscr()
     // Put the cursor at its home coordinates
     SetConsoleCursorPosition(hConsoleOutput, topLeft);
 }
-int main()
+void drawBOB(vector<pair<int, int>> &v, char a)
 {
 
+    FillConsoleOutputCharacterW(hOutput, a, 1, {offsetx + 49 + v[v.size() - 1].first, offsety + 51 + v[v.size() - 1].second}, &dwWritten);
+    FillConsoleOutputCharacterW(hOutput, a, 1, {offsetx + 51 + v[v.size() - 1].first, offsety + 51 + v[v.size() - 1].second}, &dwWritten);
+    FillConsoleOutputCharacterW(hOutput, a, 1, {offsetx + 48 + v[v.size() - 1].first, offsety + 52 + v[v.size() - 1].second}, &dwWritten);
+    FillConsoleOutputCharacterW(hOutput, a, 1, {offsetx + 50 + v[v.size() - 1].first, offsety + 52 + v[v.size() - 1].second}, &dwWritten);
+    FillConsoleOutputCharacterW(hOutput, a, 1, {offsetx + 52 + v[v.size() - 1].first, offsety + 52 + v[v.size() - 1].second}, &dwWritten);
+    FillConsoleOutputCharacterW(hOutput, a, 1, {offsetx + 49 + v[v.size() - 1].first, offsety + 53 + v[v.size() - 1].second}, &dwWritten);
+    FillConsoleOutputCharacterW(hOutput, a, 1, {offsetx + 51 + v[v.size() - 1].first, offsety + 53 + v[v.size() - 1].second}, &dwWritten);
+}
+void drawPendArm(vector<pair<int, int>> &v, char a)
+{
+
+    for (int i = 0; i < v.size(); i++)
+    {
+
+        FillConsoleOutputCharacter(hOutput, a, 1, {offsetx + xSize / 2 + v[i].first, ySize / 2 + offsety + v[i].second}, &dwWritten);
+    }
+}
+int main()
+{
     Pendulum p;
-    int a = 10;
 
     while (true)
-    {   system("CLS");
-        clrscr();
+    {
         p.x = p.length * sin(p.angle);
         p.y = p.length * cos(p.angle);
-        vector<pair<int, int>> v;
-        
-        v = drawLine(0, 0, p.x, p.y);
-        FillConsoleOutputCharacter(hOutput, char(219), 1, {offsetx + 50, 50 + offsety}, &dwWritten);
-        Sleep(10);
-        p.angle += 0.09;
-        for (int i = 0; i < v.size(); i++)
+        if (p.angle == pi / 2)
         {
-            //cout << v[i].first << " " << v[i].second << endl;
-            FillConsoleOutputCharacter(hOutput, '#', 1, {offsetx-1 + xSize / 2 + v[i].first, ySize / 2 + offsety + v[i].second}, &dwWritten);
+            p.y = 0;
         }
+        if (p.angle == 0)
+        {
+            p.x = 0;
+        }
+        float force = (g * sin(p.angle));
+        p.a = (-1 * force);
+        p.v = (p.v + p.a) * DAMPENING;
+        p.angle += p.v;
+        vector<pair<int, int>> v;
+
+        v = giveCoords(0, 0, p.x, p.y);
+        FillConsoleOutputCharacter(hOutput, char(219), 1, {offsetx + 50, 49 + offsety}, &dwWritten);
+        cout << "\n\n\n\n\n\n                                         Acceleration = " << p.a << endl;
+        cout << "                                         Angle = " << (p.angle * 180) / pi << endl;
+
+        drawPendArm(v, 'o');
+        drawBOB(v, 'x');
+        clrscr();
     }
 }
